8d95d71575da6cc83d9b42f6bc2df86c1b7197cf
868686hrk <phr8585@icloud.com>
2022-04-02 15:36:23 +0900

    タイトルシーンの中身を作成
    イメージの配置、遷移の設定

---
+++ Assets/Script/Title.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    // ƒ‰ƒCƒuƒ‰ƒŠ‚ğ’Ç‰Á
+
+    // ‘JˆÚ—pŠÖ”‚ğì¬
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene("BattleScene");
+    }
+  
+}
