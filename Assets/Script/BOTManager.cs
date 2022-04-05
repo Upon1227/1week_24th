712e8f937dcc830f676781bea33be1b9affd64fd
Upon1227 <koki20061227@gmail.com>
2022-04-05 12:06:48 +0900

    aaa

--- Assets/Script/BOTManager.cs
+++ Assets/Script/BOTManager.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class BOTManager : MonoBehaviour
 {
+    Animator anim;
     VillegeHumanManager villege;
     SpriteRenderer renderer;
     float countofsec;
     [SerializeField]int nextmove;
     [SerializeField] int movesec;
     bool isMoveFrag;
+    [SerializeField] GameObject Fukidashi;
+    [SerializeField] Text FukidashiText;
+    [SerializeField] string[] FukidashiNaiyou;
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         villege = GameObject.Find("villegemanager").GetComponent<VillegeHumanManager>();
         renderer = GetComponent<SpriteRenderer>();
         nextmove = 2;
@@ -37,7 +42,7 @@ public class BOTManager : MonoBehaviour
                     StartCoroutine(Stop());
                     isMoveFrag = true;
                 }
-
+                anim.SetInteger("Trans", 1);
                 break;
             case 2:
                 transform.position += new Vector3(0, -1f * Time.deltaTime, 0);
@@ -46,7 +51,7 @@ public class BOTManager : MonoBehaviour
                     StartCoroutine(Stop());
                     isMoveFrag = true;
                 }
-
+                anim.SetInteger("Trans",3);
                 break;
             case 3:
                 transform.position += new Vector3(1f * Time.deltaTime, 0, 0);
@@ -55,15 +60,16 @@ public class BOTManager : MonoBehaviour
                     StartCoroutine(Stop());
                     isMoveFrag = true;
                 }
-
+                anim.SetInteger("Trans", 2);
                 break;
             case 4:
-                transform.position += new Vector3(1f * Time.deltaTime, 0, 0);
+                transform.position += new Vector3(-1f * Time.deltaTime, 0, 0);
                 if (isMoveFrag == false)
                 {
                     StartCoroutine(Stop());
                     isMoveFrag = true;
                 }
+                anim.SetInteger("Trans", 4);
                 break;
             case 5:
                 if (isMoveFrag == false)
@@ -71,6 +77,7 @@ public class BOTManager : MonoBehaviour
                     StartCoroutine(Stop());
                     isMoveFrag = true;
                 }
+                anim.SetInteger("Trans", 0);
                 break;
 
         }
@@ -98,5 +105,22 @@ public class BOTManager : MonoBehaviour
         nextmove = Random.Range(1, 6);
         isMoveFrag = false;
         MoveTime();
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && Fukidashi.activeSelf == false)
+        {
+            int FukidashiSelect = Random.Range(0, FukidashiNaiyou.Length);
+            FukidashiText.text = FukidashiNaiyou[FukidashiSelect];
+            Fukidashi.SetActive(true);
+            StartCoroutine(Des());
+        }
+    }
+
+    IEnumerator Des()
+    {
+        yield return new WaitForSeconds(2);
+        Fukidashi.SetActive(false);
     }
 }
--- Assets/VillegeHumanManager.cs
+++ Assets/VillegeHumanManager.cs
@@ -5,22 +5,23 @@ using UnityEngine;
 public class VillegeHumanManager : MonoBehaviour
 {
     [SerializeField] GameObject[] BonePoint;
-    [SerializeField] GameObject Human;
+    [SerializeField] GameObject[] Human;
     // Start is called before the first frame update
     void Start()
     {
         int BoneCount = Random.Range(1, BonePoint.Length);
         for(int i = 0;i <= BoneCount; i++)
         {
-            Instantiate(Human, BonePoint[i].transform.position,Quaternion.identity);
+            int humanselect = Random.Range(0, Human.Length);
+            Instantiate(Human[humanselect], BonePoint[i].transform.position,Quaternion.identity);
         }
     }
 
    public void Bone()
     {
         int BoneCount = Random.Range(1, BonePoint.Length);
-  
-            Instantiate(Human, BonePoint[BoneCount].transform.position, Quaternion.identity);
+        int humanselect = Random.Range(0, Human.Length);
+        Instantiate(Human[humanselect], BonePoint[BoneCount].transform.position, Quaternion.identity);
      
     }
 }
